e9e921ddb09be5c19f10c1e0c8f7d7ba2698bd84
Dang3rZone <andresrosalespinzon@gmail.com>
2020-12-19 13:14:08 +0100

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f783c16a-d534-47c8-a0c6-30aac427816c';
+export const MODAL_CLOSE_SEC = 2.5;
